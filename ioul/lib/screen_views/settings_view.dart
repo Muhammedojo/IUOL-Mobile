30fd8f8bf45005cb80c37749e4a30e6a4ac7850d
Muhammad <oayodele@afexnigeria.com>
2024-05-15 12:57:54 +0100

    Added Multi language

--- ioul/lib/screen_views/settings_view.dart
+++ ioul/lib/screen_views/settings_view.dart
@@ -93,9 +93,55 @@ class SettingsView extends StatelessView<Settings, SettingsController> {
                     ],
                   ),
                 )),
+            SizedBox(
+              height: 10.h,
+            ),
             InkWell(
-                onTap: () => state.showLanguageSelectionOption(),
-                child: const Icon(Icons.lan))
+              onTap: () => state.showLanguageSelectionOption(),
+              child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.r),
+                    color: AppColors.backgroundWhite,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 2.0,
+                        spreadRadius: 0.0,
+                        offset:
+                            Offset(1.0, 1.0), // shadow direction: bottom right
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding:
+                        REdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'switch_language'.tr(),
+                          style: TextStyle(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.w600,
+                              color: const Color(0xff191C1C)),
+                        ),
+                        SizedBox(
+                          height: 8.h,
+                        ),
+                        Row(
+                          children: [
+                            Text('switch_to_another_language.'.tr(),
+                                style: TextStyle(
+                                    fontSize: 12.sp,
+                                    fontWeight: FontWeight.w400,
+                                    color: AppColors.greylight)),
+                          ],
+                        )
+                      ],
+                    ),
+                  )),
+            )
           ],
         ),
       ),
